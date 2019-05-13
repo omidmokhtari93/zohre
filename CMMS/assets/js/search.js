2a7d12ae513f3d6500c5edcba2676de07b48e4a9
IT2 <IT2@IT2>
2019-05-13 13:04:45 +0430

    search framework implemented

--- CMMS/assets/js/search.js
+++ CMMS/assets/js/search.js
@@ -1,10 +1,98 @@
 ﻿$.fn.search = function (options) {
-    var div = this;
-    var input = '<input type="text" placeholder="' + options.placeholder + '"/>';
-    div.css('width', options.width);
+  var div = this;
+  var allOpt;
+  div.css('width', options.width).addClass('search-input');
+  var result_area = $('<div name="result-area" style="width:' + this.outerWidth() + 'px;"></div>');
+  var search_lodaing = $('<img src="assets/Images/loading.png"/>');
+  var input = $('<input type="text" name="search-field" placeholder="' + options.placeholder + '"/>');
+  searchInit();
+
+  function searchInit() {
     div.append(input);
-    input.on('keyup', function () {
-        console.log(this);
-        div.append('<ul><li>یاتاقان شماره 1</li></ul>');
+    div.append(search_lodaing.hide());
+  }
+
+  var typingTimer;
+  var doneTypingInterval = 1000;
+  var $searchinput = $('#' + div.attr('id') + ' [name=search-field]');
+  $searchinput.on('keyup', function () {
+    search_lodaing.show();
+    result_area.empty();
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(doneTyping, doneTypingInterval);
+  });
+  $searchinput.on('keydown', function () {
+    clearTimeout(typingTimer);
+  });
+  function doneTyping() {
+    if ($searchinput.val().length <= 1) {
+      result_area.empty();
+      search_lodaing.hide();
+      return;
+    }
+    var par = '{"' + options.arg + '\" : \"' + $searchinput.val() + '\"}';
+    callWebApi({
+      url: options.url,
+      param: par,
+      func: appendControls
     });
+
+    function appendControls(e) {
+      var d = JSON.parse(e.d);
+      if (!d.length) {
+        return;
+      }
+      var opt = [];
+      div.append(result_area);
+      result_area.append('<input name="filter-items"/>');
+      $.each(d, function (x, y) {
+        opt.push('<li ' + options.id + '="' + y[options.id] + '">' + y[options.text] + '</li>');
+      })
+      result_area.append('<ul>' + opt.join('') + '</ul>');
+      allOpt = $('#' + div.attr('id') + ' ul li').clone();
+      search_lodaing.hide();
+    }
+  }
+
+  $('#' + div.attr('id')).on('click', 'li', function (x) {
+    div.attr('value', $(this).attr(options.id));
+    createBadge($(this).attr(options.id), $(this).text());
+    result_area.empty();
+  });
+
+  function createBadge(id, text) {
+    div.append('<span id="' + id + '">' + text + '</span>');
+    input.val('');
+    input.removeAttr('placeholder');
+  }
+
+  $('#' + div.attr('id')).on('click', 'span', function (x) {
+    $(this).remove()
+    div.removeAttr('value');
+    input.attr('placeholder', options.placeholder);
+  });
+
+
+  $('#' + div.attr('id')).on('keyup', 'input[name=filter-items]', function () {
+    var val = $(this).val();
+    $('#' + div.attr('id') + ' ul').empty();
+    allOpt.filter(function (idx, el) {
+      return val === '' || $(el).text().indexOf(val) >= 0;
+    }).appendTo('#' + div.attr('id') + ' ul');
+  });
+
+  function callWebApi(e) {
+    $.ajax({
+      type: 'POST',
+      url: e.url,
+      data: e.param,
+      contentType: 'application/json;',
+      dataType: 'json',
+      success: e.func,
+      error: function () {
+        console.log('error');
+      }
+    });
+  }
+
 };
