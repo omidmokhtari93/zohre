f6c8b18095d919cac866f50246e09f4ca0282857
IT2 <IT2@IT2>
2019-09-01 09:10:01 +0430

    minor changes

--- CMMS/assets/js/search.js
+++ CMMS/assets/js/search.js
@@ -1,101 +1,102 @@
 ﻿$.fn.search = function (options) {
-    var div = this;
-    var allOpt;
-    div.css('width', options.width).addClass('search-input');
-    var resultArea = $('<div name="result-area" style="width:' + this.outerWidth() + 'px;"></div>');
-    var searchLodaing = $('<img src="assets/Images/loading.png"/>');
-    var input = $('<input type="text" name="search-field" autocomplete="off" placeholder="' + options.placeholder + '"/>');
-    searchInit();
+  var div = this;
+  $(div).empty();
+  var allOpt;
+  div.css('width', options.width).addClass('search-input');
+  var resultArea = $('<div name="result-area" style="width:' + this.outerWidth() + 'px;"></div>');
+  var searchLodaing = $('<img src="assets/Images/loading.png"/>');
+  var input = $('<input type="text" name="search-field" autocomplete="off" placeholder="' + options.placeholder + '"/>');
+  searchInit();
 
-    function searchInit() {
-        div.append(input);
-        div.append(searchLodaing.hide());
+  function searchInit() {
+    div.append(input);
+    div.append(searchLodaing.hide());
+  }
+
+  var typingTimer;
+  var doneTypingInterval = 1000;
+  var $searchinput = $('#' + div.attr('id') + ' [name=search-field]');
+  $searchinput.on('keyup', function () {
+    searchLodaing.show();
+    resultArea.empty();
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(doneTyping, doneTypingInterval);
+  });
+  $searchinput.on('keydown', function () {
+    clearTimeout(typingTimer);
+  });
+  function doneTyping() {
+    if ($searchinput.val().length <= 1) {
+      resultArea.empty();
+      searchLodaing.hide();
+      return;
     }
-
-    var typingTimer;
-    var doneTypingInterval = 1000;
-    var $searchinput = $('#' + div.attr('id') + ' [name=search-field]');
-    $searchinput.on('keyup', function () {
-        searchLodaing.show();
-        resultArea.empty();
-        clearTimeout(typingTimer);
-        typingTimer = setTimeout(doneTyping, doneTypingInterval);
+    var par = '{"' + options.arg + '\" : \"' + $searchinput.val() + '\"}';
+    callWebApi({
+      url: options.url,
+      param: par,
+      func: appendControls
     });
-    $searchinput.on('keydown', function () {
-        clearTimeout(typingTimer);
-    });
-    function doneTyping() {
-        if ($searchinput.val().length <= 1) {
-            resultArea.empty();
-            searchLodaing.hide();
-            return;
-        }
-        var par = '{"' + options.arg + '\" : \"' + $searchinput.val() + '\"}';
-        callWebApi({
-            url: options.url,
-            param: par,
-            func: appendControls
-        });
 
-        function appendControls(e) {
-            var d = JSON.parse(e.d);
-            if (!d.length) {
-                return;
-            }
-            var opt = [];
-            div.append(resultArea);
-            resultArea.append('<input name="filter-items" placeholder="فیلتر ..."/>');
-            $.each(d, function (x, y) {
-                opt.push('<li ' + options.id + '="' + y[options.id] + '">' + y[options.text] + '</li>');
-            })
-            resultArea.append('<ul>' + opt.join('') + '</ul>');
-            allOpt = $('#' + div.attr('id') + ' ul li').clone();
-            searchLodaing.hide();
-        }
+    function appendControls(e) {
+      var d = JSON.parse(e.d);
+      if (!d.length) {
+        return;
+      }
+      var opt = [];
+      div.append(resultArea);
+      resultArea.append('<input name="filter-items" placeholder="فیلتر ..."/>');
+      $.each(d, function (x, y) {
+        opt.push('<li ' + options.id + '="' + y[options.id] + '">' + y[options.text] + '</li>');
+      })
+      resultArea.append('<ul>' + opt.join('') + '</ul>');
+      allOpt = $('#' + div.attr('id') + ' ul li').clone();
+      searchLodaing.hide();
     }
+  }
 
-    $('#' + div.attr('id')).on('click', 'li', function (x) {
-        div.attr('value', $(this).attr(options.id));
-        createBadge($(this).attr(options.id), $(this).text());
-        resultArea.empty();
-    });
+  $('#' + div.attr('id')).on('click', 'li', function (x) {
+    div.attr('value', $(this).attr(options.id));
+    createBadge($(this).attr(options.id), $(this).text());
+    resultArea.empty();
+  });
 
-    function createBadge(id, text) {
-        div.append('<span id="' + id + '">' + text + '</span>');
-        input.val('');
-        input.removeAttr('placeholder');
-        if (options.func !== undefined) {
-            options.func(id, text);
-        }
+  function createBadge(id, text) {
+    div.append('<span id="' + id + '">' + text + '</span>');
+    input.val('');
+    input.removeAttr('placeholder');
+    if (options.func !== undefined) {
+      options.func(id, text);
     }
+  }
 
-    $('#' + div.attr('id')).on('click', 'span', function (x) {
-        $(this).remove()
-        div.removeAttr('value');
-        input.attr('placeholder', options.placeholder);
+  $('#' + div.attr('id')).on('click', 'span', function (x) {
+    $(this).remove()
+    div.removeAttr('value');
+    input.attr('placeholder', options.placeholder);
+  });
+
+
+  $('#' + div.attr('id')).on('keyup', 'input[name=filter-items]', function () {
+    var val = $(this).val();
+    $('#' + div.attr('id') + ' ul').empty();
+    allOpt.filter(function (idx, el) {
+      return val === '' || $(el).text().indexOf(val) >= 0;
+    }).appendTo('#' + div.attr('id') + ' ul');
+  });
+
+  function callWebApi(e) {
+    $.ajax({
+      type: 'POST',
+      url: e.url,
+      data: e.param,
+      contentType: 'application/json;',
+      dataType: 'json',
+      success: e.func,
+      error: function () {
+        console.log('error');
+      }
     });
-
-
-    $('#' + div.attr('id')).on('keyup', 'input[name=filter-items]', function () {
-        var val = $(this).val();
-        $('#' + div.attr('id') + ' ul').empty();
-        allOpt.filter(function (idx, el) {
-            return val === '' || $(el).text().indexOf(val) >= 0;
-        }).appendTo('#' + div.attr('id') + ' ul');
-    });
-
-    function callWebApi(e) {
-        $.ajax({
-            type: 'POST',
-            url: e.url,
-            data: e.param,
-            contentType: 'application/json;',
-            dataType: 'json',
-            success: e.func,
-            error: function () {
-                console.log('error');
-            }
-        });
-    }
+  }
 
 };
--- CMMS/test.aspx
+++ CMMS/test.aspx
@@ -1,4 +1,5 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MainDesign.Master" AutoEventWireup="true" CodeBehind="test.aspx.cs" Inherits="CMMS.test" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <link href="assets/css/search.css" rel="stylesheet" />
     <script src="assets/js/search.js"></script>
@@ -7,14 +8,15 @@
             <div id="inp"></div>
         </div>
         <div class="col-md-4">
-            <input class="form-control"/>
+            <input class="form-control" />
         </div>
         <div class="col-md-4">
             <div id="mm"></div>
         </div>
     </div>
+    <button type="button" onclick="JsInit()">Initialize</button>
     <script>
-        $(function() {
+        function JsInit() {
             $('#inp').search({
                 width: '70%',
                 placeholder: 'جستجوی قطعه ...',
@@ -24,19 +26,19 @@
                 id: 'PartId',
                 func: something
             }); 
-            $('#mm').search({
-                width: '100%',
-                placeholder: 'جستجو ...',
-                url: 'WebService.asmx/FilteredGridSubSystem',
-                arg: 'subSystemName',
-                text: 'ToolName',
-                id: 'ToolId',
-                func: something
-            });
+        }    
+        $('#mm').search({
+            width: '100%',
+            placeholder: 'جستجو ...',
+            url: 'WebService.asmx/FilteredGridSubSystem',
+            arg: 'subSystemName',
+            text: 'ToolName',
+            id: 'ToolId',
+            func: something
+        });
 
-            function something(id , text) {
-                console.log(id , text)
-            }
-        })
+        function something(id , text) {
+            console.log(id , text)
+        }
     </script>
 </asp:Content>
