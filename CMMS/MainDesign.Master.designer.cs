fe3aad408e51f321cbc1d7c58386aaebc0363074
IT2 <IT2@IT2>
2020-01-25 08:24:41 +0330

    minor changes

--- CMMS/MainDesign.Master.designer.cs
+++ CMMS/MainDesign.Master.designer.cs
@@ -22,6 +22,15 @@ namespace CMMS {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Image1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
@@ -58,6 +67,51 @@ namespace CMMS {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl arshiv;
         
         /// <summary>
+        /// btn_contractor control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_contractor;
+        
+        /// <summary>
+        /// btn_stops control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_stops;
+        
+        /// <summary>
+        /// btn_measurment control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_measurment;
+        
+        /// <summary>
+        /// btnHazine control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnHazine;
+        
+        /// <summary>
+        /// btnCatalog control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCatalog;
+        
+        /// <summary>
         /// reports control.
         /// </summary>
         /// <remarks>
@@ -65,6 +119,87 @@ namespace CMMS {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl reports;
+        
+        /// <summary>
+        /// btn_MttRep control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_MttRep;
+        
+        /// <summary>
+        /// btn_machineReport control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_machineReport;
+        
+        /// <summary>
+        /// btn_PartReport control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_PartReport;
+        
+        /// <summary>
+        /// btn_RepairReport control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_RepairReport;
+        
+        /// <summary>
+        /// btn_RCFA control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_RCFA;
+        
+        /// <summary>
+        /// btn_RepairHistory control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_RepairHistory;
+        
+        /// <summary>
+        /// btn_StopReport control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_StopReport;
+        
+        /// <summary>
+        /// btn_PersonelReport control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_PersonelReport;
+        
+        /// <summary>
+        /// btn_FinancialReport control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_FinancialReport;
         
         /// <summary>
         /// machine control.
@@ -76,6 +211,60 @@ namespace CMMS {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl machine;
         
         /// <summary>
+        /// btn_device control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_device;
+        
+        /// <summary>
+        /// btn_subsystem control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_subsystem;
+        
+        /// <summary>
+        /// btn_tag control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_tag;
+        
+        /// <summary>
+        /// btn_newdevice control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_newdevice;
+        
+        /// <summary>
+        /// btn_editMachine control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_editMachine;
+        
+        /// <summary>
+        /// btn_effect control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_effect;
+        
+        /// <summary>
         /// parts control.
         /// </summary>
         /// <remarks>
@@ -83,6 +272,15 @@ namespace CMMS {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl parts;
+        
+        /// <summary>
+        /// btn_partsAcc control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_partsAcc;
         
         /// <summary>
         /// reprequest control.
@@ -94,6 +292,24 @@ namespace CMMS {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl reprequest;
         
         /// <summary>
+        /// btnRepairRequest control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRepairRequest;
+        
+        /// <summary>
+        /// btnother_repair control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnother_repair;
+        
+        /// <summary>
         /// requests control.
         /// </summary>
         /// <remarks>
@@ -103,6 +319,15 @@ namespace CMMS {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl requests;
         
         /// <summary>
+        /// show_repairRequests control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button show_repairRequests;
+        
+        /// <summary>
         /// repairdo control.
         /// </summary>
         /// <remarks>
@@ -110,6 +335,15 @@ namespace CMMS {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl repairdo;
+        
+        /// <summary>
+        /// show_allRepairs control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button show_allRepairs;
         
         /// <summary>
         /// dailyreports control.
@@ -130,6 +364,33 @@ namespace CMMS {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl DReport;
         
         /// <summary>
+        /// btn_DailyReport control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_DailyReport;
+        
+        /// <summary>
+        /// btn_showDailyReport control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_showDailyReport;
+        
+        /// <summary>
+        /// btn_dailyWorkTime control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_dailyWorkTime;
+        
+        /// <summary>
         /// services control.
         /// </summary>
         /// <remarks>
@@ -139,6 +400,51 @@ namespace CMMS {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl services;
         
         /// <summary>
+        /// btnDailyPM control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnDailyPM;
+        
+        /// <summary>
+        /// btnProgram control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnProgram;
+        
+        /// <summary>
+        /// btnDailyCM control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnDailyCM;
+        
+        /// <summary>
+        /// btnCheckedPM control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCheckedPM;
+        
+        /// <summary>
+        /// btnCheckedCm control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCheckedCm;
+        
+        /// <summary>
         /// manager control.
         /// </summary>
         /// <remarks>
@@ -146,5 +452,50 @@ namespace CMMS {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl manager;
+        
+        /// <summary>
+        /// btn_addunit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_addunit;
+        
+        /// <summary>
+        /// btn_Lines control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_Lines;
+        
+        /// <summary>
+        /// btnUserManagment control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnUserManagment;
+        
+        /// <summary>
+        /// btn_personel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_personel;
+        
+        /// <summary>
+        /// btnexit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnexit;
     }
 }
